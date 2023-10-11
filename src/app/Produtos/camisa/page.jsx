96da765a378a88ad587dfca509527034b4ae0983
jixatos <j.gody2004@gmail.com>
2023-10-11 12:11:22 -0300

    Inicialização do projeto em Next.jsx

---
+++ src/app/Produtos/camisa/page.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+        <h1>TENIS</h1>
+        
+        <div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, ducimus sunt corporis officia fugit praesentium natus in illo voluptatem. Dicta accusamus voluptate eveniet autem earum suscipit ea aperiam quis itaque?
+                Aperiam ducimus, dolorem delectus praesentium odio quos quod mollitia illum et doloribus culpa! Quae nihil minima placeat est blanditiis quod, cumque sint harum odio dignissimos commodi doloribus qui quam iste?
+            </p>
+            <p><Link href="/">Voltar...</Link></p>
+        </div>
+    </>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME</h1>
+      <div>
+        <h2>MEUS PRODUTOS</h2>
+        <ul>
+          <li><Link href="/Produtos/tenis">Tênis</Link></li>
+          <li><Link href="/Produtos/calca">Calça</Link></li>
+          <li><Link href="/Produtos/camisa">Camisa</Link></li>
+          <li><Link href="/Produtos/meia">Meia</Link></li>
+        </ul>
+      </div>
+    </>
+  )
+}
