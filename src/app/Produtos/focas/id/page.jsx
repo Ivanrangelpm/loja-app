8c8319b79d98ee57a9a304e2d28029c77a8937b9
Ivanrangelpm <rm551005@fiap.com.br>
2023-10-18 11:16:28 -0300

    fdsty

---
+++ src/app/Produtos/focas/id/page.jsx
@@ -0,0 +1,8 @@
+export default function ProdutoSlug({params}){
+    return(
+        <div>
+            <h1>Produto ID</h1>
+            <p>Slug: {params.id}</p>
+        </div>
+    )
+}
---
+++ src/app/Produtos/mosfet/slug/page.jsx
@@ -0,0 +1,8 @@
+export default function ProdutoSlug({params}){
+    return(
+        <div>
+            <h1>Produto SLU</h1>
+            <p>Slug: {params.slug}</p>
+        </div>
+    )
+}
