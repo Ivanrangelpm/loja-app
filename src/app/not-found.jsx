376f3f7a28be5e11ad711019f74e8044f3986ceb
Ivanrangelpm <rm551005@fiap.com.br>
2023-10-18 12:01:36 -0300

    not found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>
+        OOOPS! 404
+        </h1>
+        <p>Pagina nao encontrada</p>
+    </div>
+  )
+}
